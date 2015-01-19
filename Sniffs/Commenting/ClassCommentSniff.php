7ce59fb025cc9c673ce9f64afbc44b5621e96cca
Joachim Adomeit <joachim.adomeit@aoe.com>
2015-01-19 19:14:14 +0100

    Fixs to ClassComment Sniff

--- Sniffs/Commenting/ClassCommentSniff.php
+++ Sniffs/Commenting/ClassCommentSniff.php
@@ -18,6 +18,15 @@ class Ecg_Sniffs_Commenting_ClassCommentSniff extends Pear_Sniffs_Commenting_Cla
         $find[] = T_WHITESPACE;
 
         $commentEnd = $phpcsFile->findPrevious($find, ($stackPtr - 1), null, true);
+        if ($tokens[$commentEnd]['code'] !== T_DOC_COMMENT_CLOSE_TAG
+            && $tokens[$commentEnd]['code'] !== T_COMMENT
+        ) {
+            $phpcsFile->addError('Missing class doc comment', $stackPtr, 'Missing');
+            $phpcsFile->recordMetric($stackPtr, 'Class has doc comment', 'no');
+            return;
+        } else {
+            $phpcsFile->recordMetric($stackPtr, 'Class has doc comment', 'yes');
+        }
 
         // Try and determine if this is a file comment instead of a class comment.
         // We assume that if this is the first comment after the open PHP tag, then
@@ -35,7 +44,6 @@ class Ecg_Sniffs_Commenting_ClassCommentSniff extends Pear_Sniffs_Commenting_Cla
                 // This is a comment directly after the first open tag,
                 // so probably a file comment.
                 //$phpcsFile->addError('Missing class doc comment', $stackPtr, 'Missing');
-                $phpcsFile->recordMetric($stackPtr, 'Class has doc comment', 'yes');
                 //return;
             }
         }
